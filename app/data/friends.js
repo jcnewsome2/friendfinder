fa5706cf54f3ea3607b617af51c3325880b74614
Josh Newsome <joshnewsome@Joshs-MacBook-Pro.local>
2017-11-11 01:39:17 -0600

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+  {
+    name: "Donald Trump",
+    photo: "https://m.popkey.co/160d81/k08O5.gif?c=popkey-web&p=popkey&i=hotlinebling&l=direct&f=.gif",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "James Mad Dog Mattis",
+    photo: "https://i.imgur.com/1Xpuy1w.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Major Payne",
+    photo: "https://media.giphy.com/media/ilWJquzIg26Ig/giphy.gif",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Mickey Mouse",
+    photo: "https://media.giphy.com/media/M5NtWWMQpa9BC/giphy.gif",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Hillary Clinton",
+    photo: "https://media.giphy.com/media/3o6Zt4B0tZYVO7DTkk/giphy.gif",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Snoop Dogg",
+    photo: "https://media.giphy.com/media/YAKEkiuBA1erC/giphy.gif",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// export the array
+module.exports = friends;
+
